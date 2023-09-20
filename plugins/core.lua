e9940be47f91bfb7b08840820d2f86b5c32dee1a
Pranav Sindura <pranav.sindura@gmail.com>
2023-09-21 01:15:17 +0530

    add required plugins

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,116 @@ return {
       return opts
     end,
   },
+{
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require("astronvim.utils.status")
+
+    opts.statusline = { -- statusline
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+      status.component.git_branch(),
+      status.component.file_info {
+        filetype = false,
+        filename = {
+          fname = function() return vim.fn.getcwd() end, padding = { left = 1 }
+        },
+        file_modified = false,
+      },
+      status.component.git_diff(),
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      status.component.fill(),
+      status.component.lsp(),
+      status.component.treesitter(),
+      status.component.nav(),
+      status.component.mode { surround = { separator = "right" } },
+    }
+
+    opts.winbar = { -- create custom winbar
+      -- store the current buffer number
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      fallthrough = false, -- pick the correct winbar based on condition
+      -- inactive winbar
+      {
+        condition = function() return not status.condition.is_active() end,
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info {
+          file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbarnc", true),
+          surround = false,
+          update = "BufEnter",
+        },
+      },
+      -- active winbar
+      {
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info { -- add file_info to breadcrumbs
+          file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbar", true),
+          surround = false,
+          update = "BufEnter",
+        },
+        -- show the breadcrumbs
+        status.component.breadcrumbs {
+          icon = { hl = true },
+          hl = status.hl.get_attributes("winbar", true),
+          prefix = true,
+          padding = { left = 0 },
+        },
+      },
+    }
+
+    opts.tabline = { -- tabline
+      { -- file tree padding
+        condition = function(self)
+          self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+          return status.condition.buffer_matches(
+            { filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" } },
+            vim.api.nvim_win_get_buf(self.winid)
+          )
+        end,
+        provider = function(self) return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1) end,
+        hl = { bg = "tabline_bg" },
+      },
+      status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
+      status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
+      { -- tab list
+        condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
+        status.heirline.make_tablist { -- component for each tab
+          provider = status.provider.tabnr(),
+          hl = function(self)
+            return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true)
+          end,
+        },
+        { -- close button for current tab
+          provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+          hl = status.hl.get_attributes("tab_close", true),
+          von_click = { callback = function() require("astronvim.utils.buffer").close_tab() end, name = "heirline_tabline_close_tab_callback" },
+        },
+      },
+    }
+
+    opts.statuscolumn = { -- statuscolumn
+      status.component.foldcolumn(),
+        status.component.fill(),
+        status.component.numbercolumn(),
+        status.component.signcolumn(),
+      }
+
+      -- return the final configuration table
+      return opts
+    end,
+  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,27 @@
 return {
+  {
+    "tpope/vim-fugitive",
+    event = "User AstroGitFile"
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false
+  },
+  {
+    "MunifTanjim/eslint.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "jose-elias-alvarez/null-ls.nvim" },
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    config = function()
+      require("eslint").setup()
+    end
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    opts = {},
+  }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
