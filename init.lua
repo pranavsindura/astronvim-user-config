b30002298c41a52f1f755056ffd6f51a6426b99a
Pranav Sindura <pranav.sindura@gmail.com>
2023-09-21 23:42:26 +0530

    add monokai theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrotheme",
+  colorscheme = "monokai-pro",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -21,7 +21,16 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
     opts = {},
-  }
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+        require("monokai-pro").setup {
+        filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+    }
+    end
+  },
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
