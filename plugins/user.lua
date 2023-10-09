0158538727b82e9873ead1c4e7a8e3ee0f3b3ab3
Pranav Sindura <pranav.sindura@gmail.com>
2023-10-09 21:11:02 +0530

    add unimpaired for moving in quickfix list

--- plugins/user.lua
+++ plugins/user.lua
@@ -33,6 +33,10 @@ return {
     event = "User AstroFile"
   },
   {
+    "tpope/vim-unimpaired",
+    event = "User AstroFile"
+  },
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
